41090b145a07dcc4c0871ab2dc11808b7d0dd0ca
AntonyZavala <antonyzavala244@gmail.com>
2021-04-05 22:49:35 -0600

    ji

--- database/migrations/2021_04_06_044524_create_project_table.php
+++ database/migrations/2021_04_06_044524_create_project_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class Users extends Migration
+class CreateProjectTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,7 +13,12 @@ class Users extends Migration
      */
     public function up()
     {
-        //
+        Schema::create('project', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('title');
+            $table->text('description');
+            $table->timestamps();
+        });
     }
 
     /**
@@ -23,6 +28,6 @@ class Users extends Migration
      */
     public function down()
     {
-        //
+        Schema::dropIfExists('project');
     }
 }
